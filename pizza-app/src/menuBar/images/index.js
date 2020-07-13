6a73edee602fd1662033694949a44a7fd61fd4d1
Nikita <mistordi@gmail.com>
2020-07-13 13:05:30 +0300

    icon pizza

--- pizza-app/src/menuBar/images/index.js
+++ pizza-app/src/menuBar/images/index.js
@@ -1,3 +1,2 @@
-export {default as closedPizzaIcon} from "./closedPizzaIcon.png";
 export {default as openPizzaIcon} from "./openPizzaIcon.png";
 export {default as drinksIcon} from "./drinksIcon.png";
