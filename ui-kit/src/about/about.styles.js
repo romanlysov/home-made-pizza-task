eee88167fccb06b3b6e86bdc91707dcebca953a2
Даниил <84868315+DotKry@users.noreply.github.com>
2021-06-01 18:22:45 +0300

    add new about section

---
+++ ui-kit/src/about/about.styles.js
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+
+export const Span = styled.span`
+  position: absolute;
+  width: 1038px;
+  height: 334px;
+  left: 381px;
+  top: 69px;
+
+  font-family: Roboto,sans-serif;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 24px;
+  line-height: 36px;
+  text-transform: lowercase;
+`;
+
+export const Header = styled.header`
+  position: absolute;
+  width: 187px;
+  height: 36px;
+  left: 20px;
+  top: 0;
+
+  font-family: Roboto, sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 36px;
+  line-height: 36px;
+  text-transform: uppercase;
+  color: #9B5900;
+`;
+
+export const Img = styled.img`
+  position: absolute;
+  width: 320px;
+  height: 483px;
+  left: 20px;
+  top: 69px;
+`;
+
+export const Button = styled.button`
+  position: absolute;
+  width: 388px;
+  height: 94px;
+  left: 696px;
+  top: 400px;
+  background: #9B5900;
+  border-radius: 20px;
+
+  font-family: Roboto, sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 36px;
+  line-height: 36px;
+  text-align: center;
+  text-transform: uppercase;
+  color: aliceblue;
+`;
