a3c4a1b9794b583e119e0051ebe4a143269f7f6d
Даниил <84868315+DotKry@users.noreply.github.com>
2021-06-01 20:08:26 +0300

    Added catalog elements

--- ui-kit/src/index.js
+++ ui-kit/src/index.js
@@ -3,3 +3,4 @@ export { Logo } from './logo/logo.component';
 export { SmthComponent } from './smth/smth';
 export { About } from './about/about.component';
 export { Cart } from './cart/cart.component';
+export { Catalog } from './catalog/catalog.component';
