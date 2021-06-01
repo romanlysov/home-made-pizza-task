af21bf43d8ee67c2bd528bdfa08cccbe85e3150f
Nikita <nikitossuvorov@rambler.ru>
2021-06-01 19:36:06 +0300

    Footer

---
+++ ui-kit/src/footer/footer.stories.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { FooterComponent } from './footer.component';
+
+export default {
+    title: 'footer',
+};
+
+export const footer = () => <FooterComponent> </FooterComponent>;
