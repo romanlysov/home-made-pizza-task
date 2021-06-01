16e038296e950a8d9414d19f685df79725c913e0
Даниил <84868315+DotKry@users.noreply.github.com>
2021-06-01 20:05:25 +0300

    Added cart elements

--- ui-kit/src/index.js
+++ ui-kit/src/index.js
@@ -2,3 +2,4 @@ export { Heading } from './heading/heading.component';
 export { Logo } from './logo/logo.component';
 export { SmthComponent } from './smth/smth';
 export { About } from './about/about.component';
+export { Cart } from './cart/cart.component';
